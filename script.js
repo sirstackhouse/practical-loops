0f04a1fabef02b5d49347ecec4eff99ac167f140
Idris Stackhouse <idris.stackhouse@gmail.com>
2024-04-14 10:08:44 -0400

    Nested an if statement to test if a number was divisible by 3 & 5 (3 * 5 =15)

--- script.js
+++ script.js
@@ -6,6 +6,6 @@ console.log("We LIVE!")
 for (let a = 1; a < 101; a++) {
     if (a % 15 === 0) console.log("FizzBuzz");
     else if (a % 5 === 0) console.log("Buzz");
-    else if (a & 3 === 0) console.log("Fizz");
+    else if (a % 3 === 0) console.log("Fizz");
     else console.log(a);
 }
