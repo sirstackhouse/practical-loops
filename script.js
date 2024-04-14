72fd2ee40432bc48b00a5b47e4e592290977d999
Idris Stackhouse <idris.stackhouse@gmail.com>
2024-04-14 10:06:57 -0400

    Used for loop to increment up to 100

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("We LIVE!")
+
+
+///// Part 1: Fizz Buzz
+
+for (let a = 1; a < 101; a++) {
+    if (a % 15 === 0) console.log("FizzBuzz");
+    else if (a % 5 === 0) console.log("Buzz");
+    else if (a & 3 === 0) console.log("Fizz");
+    else console.log(a);
+}
