41f694a529f56978788767f2cf1ab69c4b263175
Idris Stackhouse <idris.stackhouse@gmail.com>
2024-04-14 10:10:15 -0400

    Used else if to check for number divisible by 3 and 5 and to out put "Fizz" and "Buzz" respectively

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ console.log("We LIVE!")
 
 ///// Part 1: Fizz Buzz
 
-for (let a = 1; a < 101; a++) {
+for (let a = 1; a < 101; a++) { //Loop increments to 100
     if (a % 15 === 0) console.log("FizzBuzz");
     else if (a % 5 === 0) console.log("Buzz");
     else if (a % 3 === 0) console.log("Fizz");
